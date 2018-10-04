a1b5d274b44e63f5279b8ebbb53650a0b96cb1da
Ivan Kolobaev <ikolobai@yandex.ru>
2018-10-04 16:45:24 +0300

    Мобильная шапка

---
+++ src/blocks/menu/index.js
@@ -0,0 +1,36 @@
+import './style.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('/api/menu.json')
+    .then(response => response.json())
+    .then((menu) => {
+      const menuList = document.querySelectorAll('.menu__list');
+      const menuItem = document.getElementById('menu__item');
+      for (let i = 0; i < menuList.length; i++) {
+        menu.links.forEach((link) => {
+          const itemNode = menuItem.content.cloneNode(true);
+          const linkNode = itemNode.querySelector('.menu__link');
+          linkNode.href = link.href;
+          linkNode.textContent = link.name;
+          if (link.active) {
+            linkNode.classList.add('menu__link--active');
+          }
+          menuList[0].appendChild(itemNode);
+        });
+      }
+    });
+
+  document.querySelector('.menu__burger').addEventListener('click', (event) => {
+    event.stopPropagation();
+    document.querySelector('.menu__wrapper').classList.toggle('menu__wrapper--show');
+  });
+
+  document.querySelector('.page').addEventListener('click', (event) => {
+    event.stopPropagation();
+    document.querySelector('.menu__wrapper').classList.remove('menu__wrapper--show');
+  });
+
+  document.querySelector('.menu__wrapper').addEventListener('click', (event) => {
+    event.stopPropagation();
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './variables.scss';
+import './fonts.scss';
+import './globals.scss';
+
+import './blocks/page';
+
+import './blocks/header';
+import './blocks/logo';
+import './blocks/menu';
