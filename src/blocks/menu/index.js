1bd4e0c976531a7d1d995910daccbc1ac0fc12c2
Ivan Kolobaev <ikolobai@yandex.ru>
2018-10-05 13:09:38 +0300

    fixup! Мобильная шапка

--- src/blocks/menu/index.js
+++ src/blocks/menu/index.js
@@ -3,18 +3,18 @@ import './style.scss';
 document.addEventListener('DOMContentLoaded', () => {
   fetch('/api/menu.json')
     .then(response => response.json())
-    .then(menu => {
-      const menuList = document.querySelectorAll('.menu__list');
-      for (let i = 0; i < menuList.length; i += 1) {
-        menu.links.forEach(link => {
-          const itemNode = menuList[i].getElementsByTagName('template')[0].content.cloneNode(true);
+    .then(json => {
+      const menuNodes = document.querySelectorAll('.menu__list');
+      for (let i = 0; i < menuNodes.length; i += 1) {
+        json.links.forEach(link => {
+          const itemNode = menuNodes[i].getElementsByTagName('template')[0].content.cloneNode(true);
           const linkNode = itemNode.querySelector('a');
           linkNode.href = link.href;
           linkNode.textContent = link.name;
           if (link.selected) {
             linkNode.classList.add('menu__link--selected');
           }
-          menuList[i].appendChild(itemNode);
+          menuNodes[i].appendChild(itemNode);
         });
       }
     });
