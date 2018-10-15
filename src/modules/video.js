3be40732af51831b03065712a17a18b4b4cc51d4
Ivan Kolobaev <ikolobai@yandex.ru>
2018-10-15 16:17:36 +0300

    Переписана анимация разворачивания видео не весь экран

--- src/modules/video.js
+++ src/modules/video.js
@@ -11,15 +11,10 @@ export default class Video {
     this.brightnessContainerNode = this.videoContainerNode.querySelector(
       `.${this.videoContainerClass}__range--brightness`,
     );
-    this.contrastContainerNode = this.videoContainerNode.querySelector(
-      `.${this.videoContainerClass}__range--contrast`,
-    );
-    this.brightnessNode = this.brightnessContainerNode.querySelector(
-      `.${this.videoContainerClass}__range-input`,
-    );
-    this.contrastNode = this.contrastContainerNode.querySelector(
-      `.${this.videoContainerClass}__range-input`,
-    );
+    this.contrastContainerNode = this.videoContainerNode.querySelector(`.${this.videoContainerClass}__range--contrast`);
+    this.brightnessNode = this.brightnessContainerNode.querySelector(`.${this.videoContainerClass}__range-input`);
+    this.contrastNode = this.contrastContainerNode.querySelector(`.${this.videoContainerClass}__range-input`);
+    this.curtain = document.querySelector('.page__curtain');
   }
 
   init() {
@@ -39,41 +34,54 @@ export default class Video {
 
   fullScreen(ev) {
     ev.stopPropagation();
-    if (this.isfullScreen()) {
+    if (this.isFullScreen() || this.issetFullScreen()) {
       return;
     }
     document.body.style.overflow = 'hidden';
     this.videoContainerNode.classList.add(`${this.videoContainerClass}--fullscreen`);
-    this.videoContainerNode.style.width = `${document.documentElement.clientWidth}px`;
-    this.videoContainerNode.style.height = `${document.documentElement.clientHeight}px`;
     const bounding = this.videoContainerNode.getBoundingClientRect();
+    const sX = document.documentElement.clientWidth / this.videoContainerNode.clientWidth;
+    const sY = document.documentElement.clientHeight / this.videoContainerNode.clientHeight;
+    const s = sX > sY ? sY : sX;
+    let tX = (this.videoContainerNode.clientWidth * s - this.videoContainerNode.clientWidth) / 2 - bounding.left;
+    if (document.documentElement.clientWidth > tX) {
+      tX += (document.documentElement.clientWidth - this.videoContainerNode.clientWidth * s) / 2;
+    }
+    let tY = (this.videoContainerNode.clientHeight * s - this.videoContainerNode.clientHeight) / 2 - bounding.top;
+    if (document.documentElement.clientHeight > tY) {
+      tY += (document.documentElement.clientHeight - this.videoContainerNode.clientHeight * s) / 2;
+    }
     this.videoContainerNode.style.transform = `
-          translateX(-${bounding.left}px)
-          translateY(-${bounding.top}px)`;
+      translateX(${tX}px)
+      translateY(${tY}px)
+      scale(${s})`;
+    setTimeout(() => {
+      const curtain = document.querySelector('.page__curtain');
+      curtain.style.display = 'block';
+      curtain.style.opacity = 1;
+    }, 300);
     this.videoNode.muted = false;
-    this.rollupNode.style.visibility = 'visible';
-    this.brightnessContainerNode.style.visibility = 'visible';
-    this.contrastContainerNode.style.visibility = 'visible';
   }
 
   rollup(ev) {
     ev.stopPropagation();
-    if (!this.isfullScreen()) {
+    if (!this.isFullScreen()) {
       return;
     }
-    document.body.style.overflow = '';
-    this.videoContainerNode.classList.remove(`${this.videoContainerClass}--fullscreen`);
-    this.videoContainerNode.style.width = '';
-    this.videoContainerNode.style.height = '';
-    this.videoContainerNode.style.transform = '';
     this.videoNode.muted = true;
-    this.rollupNode.style.visibility = '';
-    this.brightnessContainerNode.style.visibility = '';
-    this.contrastContainerNode.style.visibility = '';
+    this.videoContainerNode.style.transform = '';
+    this.videoContainerNode.classList.remove(`${this.videoContainerClass}--fullscreen`);
+    this.curtain.style.display = 'none';
+    this.curtain.style.opacity = 0;
+    document.body.style.overflow = '';
   }
 
-  isfullScreen() {
+  isFullScreen() {
     return this.videoContainerNode.classList.contains(`${this.videoContainerClass}--fullscreen`);
+  }
+
+  issetFullScreen() {
+    return document.querySelectorAll(`.${this.videoContainerClass}--fullscreen`).length > 0;
   }
 
   changeQuality(ev) {
