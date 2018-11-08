456b2145f495f54beec1062f7a63f8569db5d1d4
Ivan Kolobaev <ikolobai@yandex.ru>
2018-11-08 21:06:33 +0300

    Больше универсальности компонента MenuComponent
    - убран хардкор контейнера и шаблона меню

--- src/blocks/page/index.ts
+++ src/blocks/page/index.ts
@@ -9,12 +9,28 @@ const reducers = {
   menu: menuReducer,
 };
 const store = new Store(reducers);
-const menuComponent = new MenuComponent();
 
-store.subscribe(()=> {
-  const links: IItemMenuData[] = store.value.menu as IItemMenuData[];
-  menuComponent.render(links);
-});
+const menuListHeaderNode: HTMLElement | null = document.querySelector('.js-menu__list--header');
+const menuItemHeaderNode: HTMLTemplateElement | null = document.querySelector('#menuItemHeader');
+if (menuListHeaderNode && menuItemHeaderNode) {
+  const menuComponentHeader = new MenuComponent(menuListHeaderNode, menuItemHeaderNode);
+
+  store.subscribe(()=> {
+    const links: IItemMenuData[] = store.value.menu as IItemMenuData[];
+    menuComponentHeader.render(links);
+  });
+}
+
+const menuListFooterNode: HTMLElement | null = document.querySelector('.js-menu__list--footer');
+const menuItemFooterNode: HTMLTemplateElement | null = document.querySelector('#menuItemFooter');
+if (menuListFooterNode && menuItemFooterNode) {
+  const menuComponentFooter = new MenuComponent(menuListFooterNode, menuItemFooterNode);
+
+  store.subscribe(()=> {
+    const links: IItemMenuData[] = store.value.menu as IItemMenuData[];
+    menuComponentFooter.render(links);
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const menuListNode = document.querySelector<HTMLElement>('.menu__list');
--- src/components/MenuComponent.ts
+++ src/components/MenuComponent.ts
@@ -2,48 +2,30 @@ import DomHeleper from '../helpers/DomHelper';
 import IItemMenuData from '../interfaces/IItemMenuData';
 
 export default class MenuComponent {
+  private menuListNode: HTMLElement;
+  private menuItemNode: HTMLTemplateElement;
+
+  constructor(menuListNode: HTMLElement, menuItemNode: HTMLTemplateElement) {
+    this.menuListNode = menuListNode;
+    this.menuItemNode = menuItemNode;
+  }
+
   public render(links: IItemMenuData[]) {
-    const menuListHeaderNode: HTMLElement | null = document.querySelector('.js-menu__list--header');
-    if (menuListHeaderNode) {
-      menuListHeaderNode.innerHTML = '';
-    }
-    const menuListFooterNode: HTMLElement | null = document.querySelector('.js-menu__list--footer');
-    if (menuListFooterNode) {
-      menuListFooterNode.innerHTML = '';
-    }
+    this.menuListNode.innerHTML = '';
+
     links.forEach((link: IItemMenuData) => {
-      const menuItemHeader: HTMLTemplateElement | null = document.querySelector('#menuItemHeader');
-      const menuItemFooter: HTMLTemplateElement | null = document.querySelector('#menuItemFooter');
-      const menuItemHeaderTemplate: DocumentFragment | null = menuItemHeader ? DomHeleper.cloneNode(menuItemHeader.content) : null;
-      const menuItemFooterTemplate: DocumentFragment | null = menuItemFooter ? DomHeleper.cloneNode(menuItemFooter.content) : null;
+      const menuItemHeaderTemplate: DocumentFragment = DomHeleper.cloneNode(this.menuItemNode.content);
 
-      if (menuItemHeaderTemplate) {
-        const menuLinkNode: HTMLLinkElement | null = menuItemHeaderTemplate.querySelector('.menu__link');
-        if (menuLinkNode) {
-          menuLinkNode.textContent = link.name;
-          menuLinkNode.href = link.href;
-          if (link.selected) {
-            menuLinkNode.classList.add('menu__link--selected');
-          }
-        }
-
-        if (menuListHeaderNode) {
-          menuListHeaderNode.appendChild(menuItemHeaderTemplate);
+      const menuLinkNode: HTMLLinkElement | null = menuItemHeaderTemplate.querySelector('.menu__link');
+      if (menuLinkNode) {
+        menuLinkNode.textContent = link.name;
+        menuLinkNode.href = link.href;
+        if (link.selected) {
+          menuLinkNode.classList.add('menu__link--selected');
         }
       }
 
-      if (menuItemFooterTemplate) {
-        const menuLinkNode: HTMLLinkElement | null = menuItemFooterTemplate.querySelector('.menu__link');
-
-        if (menuLinkNode) {
-          menuLinkNode.textContent = link.name;
-          menuLinkNode.href = link.href;
-        }
-
-        if (menuListFooterNode) {
-          menuListFooterNode.appendChild(menuItemFooterTemplate);
-        }
-      }
+      this.menuListNode.appendChild(menuItemHeaderTemplate);
     });
   }
 }
